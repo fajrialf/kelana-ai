eefef4d9a37d93440eda2ce23f08686fd0d73584
Muhammad Fajri Alfaritsi <fajri.aflaritsi@gmail.com>
2026-08-13 20:12:06 +0700

    Add recommendation engine

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,14 @@
+from services.trip_service import calculate_daily_budget,get_trip_category, print_recommended_places, print_transportation_recommendation
 
-destination = input("Enter your destination: ")
+
+destinations =[]
+while True:
+    destination = input("Enter your destination (or leave empty to finish): ")
+    if not destination:
+        break
+
+    destinations.append(destination)
+
 country = input("Enter the country: ")
 days = int(input("Enter number of days: "))
 budget = float(input("Enter your budget: "))
@@ -11,25 +20,34 @@ transportation = float(input("Enter estimated transportation cost: "))
 miscs = float(input("Enter estimated miscellaneous cost: "))
 travel_month = input("Enter the travel month: ")
 
-
 def estimated_cost(hotel, food, transportation, miscs):
     return hotel+food+transportation+miscs
 
-def print_trip_summary(destination, days, budget, travel_style, hotel,food, transportation, miscs, country, currency, travel_month):
+def print_trip_summary(destinations, days, budget, travel_style, hotel,food, transportation, miscs, country, currency, travel_month):
+    
+
+    daily = calculate_daily_budget(1500, 5)
+    category = get_trip_category(1500)
+    total_cost = estimated_cost(hotel, food, transportation, miscs)
+    allDestinations = ""
+    for destination in destinations:
+        allDestinations+= destination + ", "
+    allDestinations = allDestinations[:-2]  # Remove the trailing comma and space
     print("===============================================================\n")
     print("Trip Summary")
     print("===============================================================\n")
-    print(f"Destination             : {destination}")
+    print(f"Destination             : {allDestinations}")
     print(f"country                 : {country}")
     print(f"Days                    : {days}")
     print(f"Budget                  : {budget:.2f} {currency}")
     print(f"Travel Style            : {travel_style}")
+    print(f"Category                : {category}")
     print(f"Travel Months           : {travel_month}")
-    total_cost = estimated_cost(hotel, food, transportation, miscs)
+    print(f"Total Estimated Cost    : {total_cost:.2f} {currency}")
+    print(f"Daily Budget            : {daily} {currency}/day")
     if (total_cost > budget):
         print(f"⚠️ exceeded budget")
-    else:
-        print(f"Total Estimated Cost    : {total_cost:.2f} {currency}")
+    print_recommended_places()
+    print_transportation_recommendation(category)
 
-
-print_trip_summary(destination, days, budget, travel_style, hotel, food, transportation, miscs, country, currency, travel_month)
+print_trip_summary(destinations, days, budget, travel_style, hotel, food, transportation, miscs, country, currency, travel_month)
---
+++ backend/services/trip_service.py
@@ -0,0 +1,31 @@
+
+def get_trip_category(budget):
+    if budget < 1000:
+        return "backpacker"
+    elif budget <= 3000:
+        return "standard"
+    else:
+        return "luxury"
+    
+def calculate_daily_budget(budget,days):
+    return budget/days
+
+def print_recommended_places():    
+    recommended_place = [
+        "Tokyo Tower",
+        "Shibuya",
+        "Mount Fuji",
+    ]
+
+    print(f"\nRecommended Places:\n")
+    for place in recommended_place:
+        print(f"- {place}")
+
+def print_transportation_recommendation(category):
+    category = category.lower()
+    if category == "backpacker":
+        print("\nRecommended Transportation: Bus")
+    elif category == "standard":
+        print("\nRecommended Transportation: Train")
+    else:
+        print("\nRecommended Transportation: Flight")
