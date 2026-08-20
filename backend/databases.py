776ba4b520b8f14e8288615b892730e1475cba6a
Muhammad Fajri Alfaritsi <fajri.aflaritsi@gmail.com>
2026-08-20 19:07:36 +0700

    Add PostgreSQL persistence

---
+++ backend/databases.py
@@ -0,0 +1,15 @@
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker,declarative_base
+import os
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_engine(DATABASE_URL)
+Sessionlocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+def init_db() -> None:
+    """Init db"""
+    Base.metadata.create_all(bind=engine)
--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,HTTPException
 from pydantic import BaseModel
 from services.trip_service import calculate_daily_budget,get_trip_category, get_transportation_recommendation,get_trip_categories, get_recommended_places,get_recommended_transportations
-
+from databases import init_db, Sessionlocal
+from models.trip import Trip
 class TripRequest(BaseModel):
     destination: str
     days: int
     budget: float
     travel_style: str
 
+init_db()
 app = FastAPI()
 
 @app.get("/")
@@ -23,13 +25,20 @@ def create_trip(request: TripRequest):
     daily_budget = calculate_daily_budget(request.budget, request.days)
     category = get_trip_category(request.budget)
     transportation = get_transportation_recommendation(category)
-    return {
-        "destination": request.destination,
-        "budget": request.budget,
-        "daily_budget": daily_budget,
-        "category": category,
-        "recommendation_transportation": transportation
-    }
+    trip = Trip(
+        destination=request.destination,
+        days=request.days,
+        budget=request.budget,
+        category=category,
+        daily_budget=daily_budget
+    )
+
+    db = Sessionlocal()
+    db.add(trip)
+    db.commit()
+    db.refresh(trip)
+    db.close()
+    return trip
 
 @app.get("/api/v1/trip-categories")
 def trip_categories():
@@ -43,3 +52,50 @@ def get_recommendations():
 @app.get("/api/v1/transportations")
 def get_transportations():
     return get_recommended_transportations()
+
+@app.get("/api/v1/trips")
+def get_trips():
+    db = Sessionlocal()
+    trips = db.query(Trip).all()
+    db.close()
+    return trips
+
+@app.get("/api/v1/trips/{trip_id}")
+def get_trip(trip_id :int):
+    db = Sessionlocal()
+    trip = db.query(Trip).filter(Trip.id == trip_id).first()
+    db.close()
+    if trip is None:
+        raise HTTPException(status_code=404, detail=f"Trip with id {trip_id} not found")
+    return trip
+
+@app.delete("/api/v1/trips/{trip_id}", status_code=204)
+def delete_trip(trip_id  :int):
+    db = Sessionlocal()
+    trip = db.query(Trip).filter(Trip.id == trip_id).first()
+    if trip is None:
+        raise HTTPException(status_code=404, detail=f"Trip with id {trip_id} not found")
+    db.delete(trip)
+    db.commit()
+    db.close()
+
+@app.put("/api/v1/trips/{trip_id}")
+def update_trip(trip_id: int, request: TripRequest):
+    db = Sessionlocal()
+    trip = db.query(Trip).filter(Trip.id == trip_id).first()
+    if trip is None:
+        raise HTTPException(status_code=404, detail=f"Trip with id {trip_id} not found")
+    
+    daily_budget = calculate_daily_budget(request.budget, request.days)
+    category = get_trip_category(request.budget)
+    
+    trip.destination = request.destination
+    trip.days = request.days
+    trip.budget = request.budget
+    trip.daily_budget = daily_budget
+    trip.category = category
+
+    db.commit()
+    db.refresh(trip)
+    db.close()
+    return trip
